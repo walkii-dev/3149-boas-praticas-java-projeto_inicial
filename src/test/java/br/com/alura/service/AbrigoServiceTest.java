85672c9a8283a058ff3f411abaf031159b7d0fae
Walkii-dev <lucasp.oliveira822@gmail.com>
2026-08-26 00:12:23 -0300

    implementing first test (done)

---
+++ src/test/java/br/com/alura/service/AbrigoServiceTest.java
@@ -0,0 +1,51 @@
+package br.com.alura.service;
+
+import br.com.alura.configuration.HttpClientConfiguration;
+import br.com.alura.model.Abrigo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.http.HttpResponse;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AbrigoServiceTest {
+    // a instancia da classe HttpClientConfiguration é uma simulação. (que obviamente não é o retorno comum).
+    private HttpClientConfiguration configuration = mock(HttpClientConfiguration.class);
+    // é declarado a classe que será testada aqui (com a informação previamente mockada)
+    private AbrigoService abrigoService = new AbrigoService(configuration);
+    //como é uma resposta de api externa, para fazer o teste é necessário simular.
+    private HttpResponse<String> response = mock(HttpResponse.class);
+    // criando uma instancia da entidade para o teste.
+    private Abrigo abrigo = new Abrigo("Teste","21983177667","abrigo_teste@gmail.com");
+
+    @Test
+    public void mustVerifyIfDoGetRequestBeCalled() throws IOException, InterruptedException {
+        abrigo.setId(0l);
+
+        // resultados que devem ser esperados de ocorrer neste teste
+        String expectedAbrigosCadastrados = "Abrigos cadastrados:";
+        String expectedIdAndName = "0 - Teste";
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream printStream  = new PrintStream(baos);
+        System.setOut(printStream);
+
+        when(response.body()).thenReturn("[{"+abrigo.toString()+"}]");
+        when(configuration.fazerRequisicaoGet(anyString())).thenReturn(response);
+
+        abrigoService.listarAbrigos();
+
+        String[] lines = baos.toString().split(System.lineSeparator());
+        String actualAbrigosCadastrados = lines[0];
+        String actualIdAndNome = lines[1];
+
+        Assertions.assertEquals(expectedAbrigosCadastrados,actualAbrigosCadastrados);
+        Assertions.assertEquals(expectedIdAndName,actualIdAndNome);
+    }
+}
